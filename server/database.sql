ab7d6ef06d9e99537085c937a69136ebf59cf79b
Chirag Jain <cjchirag7@gmail.com>
2020-10-19 16:19:37 +0530

    Updated DB Tables to include AUTO_INCREMENT on primary key - id Also include created_at by DEFAULT

--- server/database.sql
+++ server/database.sql
@@ -1,134 +1,145 @@
-CREATE DATABASE  IF NOT EXISTS `cl_judge` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `cl_judge`;
--- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
---
--- Host: localhost    Database: cl_judge
--- ------------------------------------------------------
--- Server version	8.0.21-0ubuntu0.20.04.4
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- -----------------------------------------------------
+-- Schema cl_judge
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cl_judge` ;
 
---
--- Table structure for table `groups`
---
+-- -----------------------------------------------------
+-- Schema cl_judge
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cl_judge` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `cl_judge` ;
 
-DROP TABLE IF EXISTS `groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `groups` (
-  `id` varchar(45) NOT NULL,
-  `group_name` varchar(45) NOT NULL,
-  `confidential` tinyint NOT NULL,
-  `creator` varchar(45) NOT NULL,
-  `member_count` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `users` ;
 
---
--- Table structure for table `notifications`
---
-
-DROP TABLE IF EXISTS `notifications`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notifications` (
-  `id` varchar(45) NOT NULL,
-  `heading` varchar(110) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `created_at` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user_groups`
---
-
-DROP TABLE IF EXISTS `user_groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_groups` (
-  `id` varchar(45) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `group_id` varchar(45) NOT NULL,
-  `is_group_moderator` tinyint NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `user_groups_fk_username_idx` (`username`),
-  KEY `user_groups_fk_group_id_idx` (`group_id`),
-  CONSTRAINT `user_groups_fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `user_groups_fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user_notifications`
---
-
-DROP TABLE IF EXISTS `user_notifications`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_notifications` (
-  `id` varchar(45) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `notification_id` varchar(45) NOT NULL,
-  `read`  tinyint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `user_notifications_fk_username_idx` (`username`),
-  KEY `user_notifications_fk_notification_id_idx` (`notification_id`),
-  CONSTRAINT `user_notifications_fk_notification_id` FOREIGN KEY (`notification_id`) REFERENCES `notifications` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `user_notifications_fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `username` varchar(45) NOT NULL,
-  `secret` longtext NOT NULL,
-  `full_name` varchar(45) NOT NULL,
-  `admission_number` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `mobile` varchar(45) DEFAULT NULL,
-  `is_admin` tinyint DEFAULT NULL,
-  `otp` varchar(45) DEFAULT NULL,
-  `otp_valid_upto` varchar(45) DEFAULT NULL,
-  `verified` tinyint DEFAULT NULL,
-  `department` int NOT NULL,
-  `course` int NOT NULL,
-  `bio` varchar(110) DEFAULT NULL,
-  `profile_img` varchar(110) DEFAULT NULL,
-  `admission_year` int NOT NULL,  
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` VARCHAR(45) NOT NULL,
+  `secret` LONGTEXT NOT NULL,
+  `full_name` VARCHAR(45) NOT NULL,
+  `admission_number` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `mobile` VARCHAR(45) NULL DEFAULT NULL,
+  `is_admin` TINYINT NULL DEFAULT NULL,
+  `otp` VARCHAR(45) NULL DEFAULT NULL,
+  `otp_valid_upto` VARCHAR(45) NULL DEFAULT NULL,
+  `verified` TINYINT NULL DEFAULT NULL,
+  `department` INT NOT NULL,
+  `course` INT NOT NULL,
+  `bio` VARCHAR(110) NULL DEFAULT NULL,
+  `profile_img` VARCHAR(110) NULL DEFAULT NULL,
+  `admission_year` INT NOT NULL,
   PRIMARY KEY (`username`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-17 18:49:13
+-- -----------------------------------------------------
+-- Table `groups`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `groups` ;
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_name` VARCHAR(45) NOT NULL,
+  `confidential` TINYINT NOT NULL,
+  `creator` VARCHAR(45) NOT NULL,
+  `member_count` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `groups_fk_creator` (`creator` ASC),
+  CONSTRAINT `groups_fk_creator`
+    FOREIGN KEY (`creator`)
+    REFERENCES `users` (`username`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `notifications`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `notifications` ;
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `heading` VARCHAR(110) NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `user_groups`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_groups` ;
+
+CREATE TABLE IF NOT EXISTS `user_groups` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `group_id` INT NOT NULL,
+  `is_group_moderator` TINYINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `user_groups_fk_username_idx` (`username` ASC),
+  INDEX `user_groups_fk_group_id_idx` (`group_id` ASC),
+  CONSTRAINT `user_groups_fk_group_id`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `groups` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `user_groups_fk_username`
+    FOREIGN KEY (`username`)
+    REFERENCES `users` (`username`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `user_notifications`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_notifications` ;
+
+CREATE TABLE IF NOT EXISTS `user_notifications` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `notification_id` INT NOT NULL,
+  `read` TINYINT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `user_notifications_fk_username_idx` (`username` ASC),
+  INDEX `user_notifications_fk_notification_id_idx` (`notification_id` ASC),
+  CONSTRAINT `user_notifications_fk_notification_id`
+    FOREIGN KEY (`notification_id`)
+    REFERENCES `notifications` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `user_notifications_fk_username`
+    FOREIGN KEY (`username`)
+    REFERENCES `users` (`username`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
