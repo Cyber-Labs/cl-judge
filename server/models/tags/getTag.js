67eec84e5b5d4c88d6af334facf949b09173d4d3
ridhishjain <ultimate.ridhish@gmail.com>
2020-12-12 21:12:35 +0530

    little fix (#76)

--- server/models/tags/getTag.js
+++ server/models/tags/getTag.js
@@ -11,16 +11,23 @@ function getTag({ query }) {
   return new Promise((resolve, reject) => {
     const keyword = query.keyword || ''
     const searchId = query.id || ''
-    pool.query(
-      `SELECT * FROM tags WHERE id LIKE ? AND name LIKE ?`,
-      [`%${searchId}%`, `%${keyword}%`],
-      (error, results) => {
-        if (error || results === undefined) {
-          return reject(error)
-        }
-        return resolve(results)
+    let poolQuery = ''
+    let arr = []
+    if (searchId && searchId !== '') {
+      poolQuery += `SELECT * FROM tags WHERE id = ?`
+      arr.push(searchId)
+    } else if (keyword && keyword !== '') {
+      poolQuery += `SELECT * FROM tags WHERE name LIKE ?`
+      arr.push(`%${keyword}%`)
+    } else {
+      poolQuery += `SELECT * FROM tags`
+    }
+    pool.query(poolQuery, arr, (error, results) => {
+      if (error || results === undefined) {
+        return reject(error)
       }
-    )
+      return resolve(results)
+    })
   })
 }
 
