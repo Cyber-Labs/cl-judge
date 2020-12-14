cd18788b69c271a40f77d285e50206302585fad2
ridhisjain <ultimate.ridhish@gmail.com>
2020-12-14 17:05:14 +0530

    getter contest APIs

--- server/models/contests/index.js
+++ server/models/contests/index.js
@@ -4,6 +4,13 @@ const addModerator = require('./addModerator')
 const removeModerator = require('./removeModerator')
 const addGroup = require('./addGroup')
 const removeGroup = require('./removeGroup')
+const getContest = require('./getContest')
+const getContests = require('./getContests')
+const getModeratorContests = require('./getModeratorContests')
+const getContestModerators = require('./getContestModerators')
+const participate = require('./participate')
+const getAllParticipants = require('./getAllParticipants')
+const getAllParticipantsDetails = require('./getAllParticipantsDetails')
 
 module.exports = {
   createContest,
@@ -12,4 +19,11 @@ module.exports = {
   removeModerator,
   addGroup,
   removeGroup,
+  getContest,
+  getContests,
+  getModeratorContests,
+  getContestModerators,
+  participate,
+  getAllParticipants,
+  getAllParticipantsDetails,
 }
--- server/routes/contests/index.js
+++ server/routes/contests/index.js
@@ -4,6 +4,13 @@ const addModeratorRouter = require('./addModerator')
 const removeModeratorRouter = require('./removeModerator')
 const addGroupRouter = require('./addGroup')
 const removeGroupRouter = require('./removeGroup')
+const getContestRouter = require('./getContest')
+const getContestsRouter = require('./getContests')
+const getModeratorContestsRouter = require('./getModeratorContests')
+const getContestModeratorsRouter = require('./getContestModerators')
+const participateRouter = require('./participate')
+const getAllParticipantsRouter = require('./getAllParticipants')
+const getAllParticipantsDetailsRouter = require('./getAllParticipantsDetails')
 
 module.exports = {
   createContestRouter,
@@ -12,4 +19,11 @@ module.exports = {
   removeModeratorRouter,
   addGroupRouter,
   removeGroupRouter,
+  getContestRouter,
+  getContestsRouter,
+  getModeratorContestsRouter,
+  getContestModeratorsRouter,
+  participateRouter,
+  getAllParticipantsRouter,
+  getAllParticipantsDetailsRouter,
 }
