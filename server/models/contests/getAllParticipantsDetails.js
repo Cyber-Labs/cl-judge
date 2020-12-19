1335688923d0a22d90d5fe388f927bbb602ce494
ridhishjain <ultimate.ridhish@gmail.com>
2020-12-19 14:53:40 +0530

    getter contest APIs (#80)
    * Getter contest APIs
    
    Co-authored-by: Chirag Jain <cjchirag7@gmail.com>
    
    Co-authored-by: NIKHIL BN <45630095+NBNARADHYA@users.noreply.github.com>

---
+++ server/models/contests/getAllParticipantsDetails.js
@@ -0,0 +1,35 @@
+const { pool } = require('../database')
+
+/**
+ *
+ * @param {*} param0
+ * @param {String} param0.username
+ * @param {Object} param0.params
+ * @return {Promise}
+ *
+ */
+
+function getAllParticipantsDetails({ username, params }) {
+  return new Promise((resolve, reject) => {
+    const { contest_id: contestId } = params
+    pool.query(
+      `SELECT username, full_name, admission_number, email, mobile, department, course, admission_year, profile_img FROM users u
+      INNER JOIN  contests_participants cp ON u.username = cp.participant 
+      WHERE EXISTS(SELECT 1 FROM contests_moderators WHERE contest_id=? AND moderator=?) AND cp.contest_id=?`,
+      [contestId, username, contestId],
+      (error, results) => {
+        if (error || results === undefined) {
+          return reject(error)
+        }
+        if (!results.length) {
+          return reject(
+            'Either no one has yet participated or the user do not have required permissions'
+          )
+        }
+        return resolve(results)
+      }
+    )
+  })
+}
+
+module.exports = getAllParticipantsDetails
