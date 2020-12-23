63be0e644504ca2392663e3a8bea0587e766e56b
ridhishjain <ultimate.ridhish@gmail.com>
2020-12-23 11:11:52 +0530

    Update server/models/contests/getAllQuestions.js
    Co-authored-by: Chirag Jain <cjchirag7@gmail.com>

--- server/models/contests/getAllQuestions.js
+++ server/models/contests/getAllQuestions.js
@@ -12,9 +12,11 @@ function getAllQuestions({ username, params }) {
     const { contest_id: contestId } = params
     pool.query(
       `SELECT question_id, q.name, q.difficulty, q.type, max_score FROM contests_questions cq
-      INNER JOIN contests c ON c.id = cq.contest_id WHERE (c.public = 1)
-      OR (EXISTS(SELECT 1 FROM contests_groups cg INNER JOIN user_groups ug ON cg.group_id = ug.group_id 
-        WHERE ug.username = ? AND cg.contest_id = ?))`,
+      INNER JOIN contests c ON c.id = cq.contest_id
+      INNER JOIN questions q ON cq.question_id=q.id
+      WHERE c.id=? AND (EXISTS(SELECT 1 from contests_moderators WHERE contest_id=? AND moderator=?) OR (NOW()>c.end_time AND c.confidential_questions=0 AND (c.public = 1
+      OR EXISTS(SELECT 1 FROM contests_groups cg INNER JOIN user_groups ug ON cg.group_id = ug.group_id 
+        WHERE ug.username = ? AND cg.contest_id = ?))) OR (NOW()>=c.start_time AND NOW()<=c.end_time AND EXISTS(SELECT 1 FROM contests_participants WHERE contest_id=? AND participant=?)))`,
       [username, contestId],
       (error, results) => {
         if (error || results === undefined) {
