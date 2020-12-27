0e35305411325b6ea061cd32e6a2306b64dc1537
ridhishjain <ultimate.ridhish@gmail.com>
2020-12-27 11:29:44 +0530

    Update server/models/contests/removeQuestion.js
    Co-authored-by: NIKHIL BN <45630095+NBNARADHYA@users.noreply.github.com>

--- server/models/contests/removeQuestion.js
+++ server/models/contests/removeQuestion.js
@@ -14,7 +14,7 @@ function removeQuestion({ params, body, username }) {
     const { question_id: questionId } = body
     pool.query(
       `DELETE FROM contests_questions WHERE (SELECT 1 FROM contests_moderators WHERE contest_id=? AND moderator=?) AND question_id = ? AND contest_id = ?`,
-      [questionId, contestId, username],
+      [contestId, username, questionId, contestId],
       (error, res) => {
         if (error || res === undefined) {
           return reject(error)
